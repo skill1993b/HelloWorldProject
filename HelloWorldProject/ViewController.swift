5e8071cf1d38a9ff467b8177b078c9cd960ca96c
Nikita Zhuravlev <skill1993@icloud.com>
2021-05-17 22:47:14 +0300

    Set Button and Labels

--- HelloWorldProject/ViewController.swift
+++ HelloWorldProject/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var showMeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        showMeButton.layer.cornerRadius = 10
     }
 
-
+    
+    @IBAction func showMeButtonTapped(_ sender: Any) {
+        
+        
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden {
+            showMeButton.setTitle("Show Me!", for: .normal)
+        } else {
+            showMeButton.setTitle("Hide Me!", for: .normal)
+        }
+        
+    }
+    
 }
 
